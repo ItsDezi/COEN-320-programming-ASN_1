d3bec971b149ae2bc82915c62dedfbc15e9fefef
ItsDezi <j_desman@live.concordia.ca>
2023-05-15 19:06:57 -0400

    back button and toolbar from settings page

--- app/src/main/java/com/example/programming_asn_1/SettingsActivity.java
+++ app/src/main/java/com/example/programming_asn_1/SettingsActivity.java
@@ -1,8 +1,11 @@
 package com.example.programming_asn_1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingsActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
